9f0fc4838dfdb097728b7fbb8fabb398c37d5531
epellis <nedellis1234@gmail.com>
2016-02-12 12:54:49 -0500

    Activity 1 Complete
    Added in some constructors and accessors

---
+++ ElevensLab/CardTester.java
@@ -0,0 +1,23 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card cardOne = new Card("Nine", "Spaids", 9);
+        Card cardTwo = new Card("Seven", "Hearts", 7);
+        Card cardThree = new Card("King", "Clubs", 10);
+
+        System.out.println(cardOne.matches(cardTwo));
+        System.out.println(cardOne.matches(cardThree));
+        System.out.println(cardOne.rank());
+        System.out.println(cardThree.pointValue());
+        System.out.println(cardTwo.suit());
+        System.out.println(cardOne.toString());
+    }
+}
