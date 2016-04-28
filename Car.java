03cdc03a66a205551f875658a2560df7694d8775
Anthony Romaniello <aromaniello70@gmail.com>
2016-04-28 09:39:34 -0400

    Added files via upload

---
+++ Car.java
@@ -0,0 +1,13 @@
+
+public class Car extends Vehicle {
+
+    //add code for constructor and any needed methods
+    public Car(String mod) {
+        super(mod);
+    }
+
+    public String sound() {
+        return "zoom";
+    }
+
+}
---
+++ Truck.java
@@ -0,0 +1,13 @@
+
+public class Truck extends Vehicle {
+
+    //add code for constructor and any needed methods
+    public Truck(String mod) {
+        super(mod);
+    }
+
+    public String sound() {
+        return "rumble";
+    }
+
+}
